c1b2a993717a4da805b7860f84c0f7beee741c39
bhavya <bhavya2313139@akgec.ac.in>
2025-04-14 12:54:32 +0530

    updated the project files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,27 @@ import React, { useState, useEffect } from "react";
 import { getPosts,getRandomUser } from "./api";
 import "./App.css";
 import PostCard from "./components/PostCard";
+import UserCard from "./components/UserCard";
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [userData,setUserData] = useState(null);
 
   useEffect(() => {
     getPosts().then((posts) => setData(posts));
   }, []);
+  
+  useEffect(() =>{
+     getRandomUser().then((user) => setUserData(user.results[0]))
+  },[]);
+
+  console.log(userData)
 
   return (
     <div className="App">
+     { userData &&  <UserCard data={userData}/>} 
       {
+       
         data
           ? data.map((e) => <PostCard title ={e.title} body= {e.body}/>)
           : <p>No data</p>
