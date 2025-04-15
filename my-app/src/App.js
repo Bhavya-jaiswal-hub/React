37a29139d8412475c559459a91e788589110b738
bhavya <bhavya2313139@akgec.ac.in>
2025-04-16 01:06:34 +0530

    updated the react files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { getPosts,getRandomUser } from "./api";
+// import React, { useState, useEffect } from "react";
+// import { getPosts,getRandomUser } from "./api";
+import React from 'react'
+import { useContext } from 'react';
 import "./App.css";
-import PostCard from "./components/PostCard";
-import UserCard from "./components/UserCard";
+import Counter from './components/Counter';
+import { ConterContext}  from "./Contexts/Counter"
+// import PostCard from "./components/PostCard";
+// import UserCard from "./components/UserCard";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [userData,setUserData] = useState(null);
+  const counterState = useContext(ConterContext)
+  // const [data, setData] = useState(null);
+  // const [userData,setUserData] = useState(null);
 
-  useEffect(() => {
-    getPosts().then((posts) => setData(posts));
-  }, []);
+  // useEffect(() => {
+  //   getPosts().then((posts) => setData(posts));
+  // }, []);
   
-  useEffect(() =>{
-     getRandomUser().then((user) => setUserData(user.results[0]))
-  },[]);
+  // useEffect(() =>{
+  //    getRandomUser().then((user) => setUserData(user.results[0]))
+  // },[]);
 
-  const refresh = () => {
-     getRandomUser().then((user) => setUserData(user.results[0]))
-  };
+  // const refresh = () => {
+  //    getRandomUser().then((user) => setUserData(user.results[0]))
+  // };
 
   return (
     <div className="App">
-      <button onClick={refresh}>Refresh User</button>
-     { userData &&  <UserCard data={userData}/>} 
-      {
-       
-        data
-          ? data.map((e) => <PostCard title ={e.title} body= {e.body}/>)
-          : <p>No data</p>
-      }
+       <h1>
+        Count is {counterState.count}
+       </h1>
+
+       <Counter/>
+       <Counter/>
+       <Counter/>
+       <Counter/>
+
     </div>
   );
 };
---
+++ my-app/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Counter = () => {
+     return (
+        <div>
+            <button>
+                Increment
+            </button>
+            <button>
+                Increment
+            </button>
+        </div>
+     );
+};
+
+
+export default Counter;
