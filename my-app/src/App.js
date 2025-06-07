7de2cd552f95276f7fcbb9cf48389b8d42ba3cc0
bhavya <bhavya2313139@akgec.ac.in>
2025-06-07 10:41:12 +0530

    created the tic tac game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,61 +1,12 @@
-// import React, { useState, useEffect } from "react";
-// import { getPosts,getRandomUser } from "./api";
-import React from 'react'
-// import Card from './components/Card'
-// import Button from './components/Button'
-// import Input from './components/Input'
-// import Item from './components/Item';
-// import Cart from './components/Cart';
-// import { useContext } from 'react';
-// import "./App.css";
-// import { useWeather } from './Context/Weather';
-// import Counter from './components/Counter';
-// import { ConterContext}  from "./Contexts/Counter"
-// import PostCard from "./components/PostCard";
-// import UserCard from "./components/UserCard";
 
+import './App.css'
+import Board from "./TicTacToeGame/Board";
 const App = () => {
 
-  // const weather = useWeather();
-  // console.log(weather)
-  // const counterState = useContext(ConterContext)
-  // const [data, setData] = useState(null);
-  // const [userData,setUserData] = useState(null);
-
-  // useEffect(() => {
-  //   getPosts().then((posts) => setData(posts));
-  // }, []);
-  
-  // useEffect(() =>{
-  //    getRandomUser().then((user) => setUserData(user.results[0]))
-  // },[]);
-
-  // const refresh = () => {
-  //    getRandomUser().then((user) => setUserData(user.results[0]))
-  // };
-
+ 
   return (
     <div className="App">
-      {/* <Item name="Macbook pro"  price={100000}/>
-      <Item name="Pen drive"  price={40000}/>
-      <Item name="Mobile"  price={350000}/> */}
-      {/* <Cart/> */}
-       {/* <h1>
-        Count is {counterState.count}
-       </h1>
-
-       <Counter/>
-       <Counter/>
-       <Counter/>
-       <Counter/> */}
-
-       {/* <h1> Weather forcast</h1> */}
-       {/* <Input/> */}
-       {/* <br></br> */}
-       {/* <Button onClick={weather.fetchData} value="Search"/>
-       <Card/>
-       <Button value="Refresh"/> */}
-    <h1> React Router</h1>
+     <Board / >
     </div>
   );
 };
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,109 +1,11 @@
-import React, { useEffect,useState }from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-// import { WeatherProvider } from './Context/Weather';
-
-
-
-import {BrowserRouter,Routes,Route,useParams,NavLink} from "react-router-dom"
-
-
-// import { CartProvider } from './Context/Cart';
+import App from './App';
+import './index.css'; // Optional: if you want to use global CSS styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const Home = () => {
-
-  const [posts,setPosts] =useState([])
-
-  useEffect(()=> {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((data) => data.json())
-    .then((data) => setPosts(data));
-
-  },[])
-   return (
-    <div className='post-container'>
-    {
-       posts.map((post) => ({ <NavLink to={'/post'}>post.title} </NavLink>) )
-    }
-    </div>
-    
-   )
-}
-
-const About = () => {
-   return (
-    <div>
-     <h1> 
-      About Page
-     </h1>
-    </div>
-   )
-}
-
-const Profile = () => {
-   return (
-    <div>
-      <h1>
-        settings 
-      </h1>
-    </div>
-   )
-}
-
-const Settings = () => {
-   return (
-    <div>
-      <h1>
-        sex karne ka bahut shok h mujhe 
-      </h1>
-    </div>
-   )
-}
-
-const UserName = () => {
-
-  const params = useParams();
-   return (
-     <div>
-      <h1> Your name is {params.UserId}</h1>
-     </div>
-   )
-}
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
-   <Routes>
-     <Route path="/" element={<Home />} />
-     <Route path="/about" element={<About />} />
-     <Route path ="/aboutUser/:UserId"  element={ <UserName />} /> 
-      {/* <Route path="/account/profile"  element={<Profile />} /> */}
-     <Route path="account">
-      <Route path="profile"  element = {<Profile />} />
-      <Route path="sextalk"  element = { <Settings/>} />
-
-     </Route>
-
-   </Routes>
-   {/* <App/> */}
-   </BrowserRouter>
-    {/* <CartProvider>
-      <App />
-    </CartProvider> */}
-
-    
-  {/* <WeatherProvider>
-  <App/>
-  </WeatherProvider> */}
- 
-  
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
