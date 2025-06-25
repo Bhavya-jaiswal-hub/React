909b8411c669aa6f3ab896a63d3b94080fe3e5fc
bhavya <bhavya2313139@akgec.ac.in>
2025-06-25 22:07:59 +0530

    updated the code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,24 @@
 // import {getPosts,getRandomUser}  from './api'
 //  import {getDatabase ,ref,set} from "firebase/database"
-import {getAuth , createUserWithEmailAndPassword} from "firebase/auth"
- import {app}  from  "./firebase"
+// import {getAuth , createUserWithEmailAndPassword} from "firebase/auth"
+import Signup from "./pages/signuppage";
+//  import {app}  from  "./firebase"
  // import {useState,useEffect}  from 'react';
 // import UserCard  from './components/UserCard';
 import './App.css'
 
- const auth = getAuth(app);
+//  const auth = getAuth(app);
 // import Card from "./components/Card";
 // import Input from './components/Input';
 // import PostCard from   './components/PostCard';
 // import Board from "./TicTacToeGame/Board";
 // const db = getDatabase(app) // this db will give you the instance of the database of the firebase 
 const App = () => {
-  
+
  
-    const signupUser  = () => {
-       createUserWithEmailAndPassword(auth,"bhavyajaiswal71@gmai.com","bhav@1234").then((value) => console.log(value));
-    }
+    // const signupUser  = () => {
+    //    createUserWithEmailAndPassword(auth,"bhavyajaiswal71@gmai.com","bhav@1234").then((value) => console.log(value));
+    // }
   // const [data,setData] = useState(null); 
   // const [userdata,setUserdata] = useState(null);
   
@@ -42,8 +43,9 @@ const App = () => {
     // };
   return (
     <div className="App">
-       <h1> Firebase project  </h1> 
-       <button className="btn" onClick={signupUser}>create user</button> 
+       {/* <h1> Firebase project  </h1> 
+       <button className="btn" onClick={signupUser}>create user</button>  */}
+       <Signup/>
      {/* <Input placeholder="search the country" />
      <Card /> */}
       {/* {userdata &&  <UserCard data={userdata} /> }
