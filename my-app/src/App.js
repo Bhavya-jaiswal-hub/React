0e707fa2e15bacee9f941bab1e461f0ed9fc5ea5
bhavya <bhavya2313139@akgec.ac.in>
2025-06-25 19:28:15 +0530

    updatd the code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,23 @@
 // import {getPosts,getRandomUser}  from './api'
- import {getDatabase ,ref,set} from "firebase/database"
+//  import {getDatabase ,ref,set} from "firebase/database"
+import {getAuth , createUserWithEmailAndPassword} from "firebase/auth"
  import {app}  from  "./firebase"
  // import {useState,useEffect}  from 'react';
 // import UserCard  from './components/UserCard';
 import './App.css'
+
+ const auth = getAuth(app);
 // import Card from "./components/Card";
 // import Input from './components/Input';
 // import PostCard from   './components/PostCard';
 // import Board from "./TicTacToeGame/Board";
-const db = getDatabase(app) // this db will give you the instance of the database of the firebase 
+// const db = getDatabase(app) // this db will give you the instance of the database of the firebase 
 const App = () => {
   
+ 
+    const signupUser  = () => {
+       createUserWithEmailAndPassword(auth,"bhavyajaiswal71@gmai.com","bhav@1234").then((value) => console.log(value));
+    }
   // const [data,setData] = useState(null); 
   // const [userdata,setUserdata] = useState(null);
   
@@ -26,17 +33,17 @@ const App = () => {
   //     getRandomUser().then((user) => setUserdata(user.results[0])); 
   // };
  
-    const putData = () => {
-       set(ref  (db,"users/bhavya"),{
-         id: 1,
-         name: "bhavya",
-         age: 21,
-       });
-    };
+    // const putData = () => {
+    //    set(ref  (db,"users/bhavya"),{
+    //      id: 1,
+    //      name: "bhavya jaiswal",
+    //      age: 21,
+    //    });
+    // };
   return (
     <div className="App">
-      <h1> Firebase project  </h1>
-      <button className="btn" onClick={putData}>Put Data</button>
+       <h1> Firebase project  </h1> 
+       <button className="btn" onClick={signupUser}>create user</button> 
      {/* <Input placeholder="search the country" />
      <Card /> */}
       {/* {userdata &&  <UserCard data={userdata} /> }
